5a6a5735887d0ea9a7f7d775364389bfeccfd3e2
badCatCoder <badCatCoder@gmail.com>
2020-04-07 18:05:07 +0200

    fine es

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,6 +1,6 @@
 package it.polito.tdp.parole.model;
 
-// import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,23 +11,24 @@ public class Parole {
 	
 	public Parole() {
 		lista = new LinkedList<String>();
-		// lista = new ArrayList<String>();
+		//lista = new ArrayList<String>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
 		lista.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
 		Collections.sort(lista);
 		return lista;
 	}
 	
 	public void reset() {
-		// TODO
 		lista.clear();
 	}
 	
+	public void removeParola(String string) {
+		lista.remove(string);
+	}
+
 }
